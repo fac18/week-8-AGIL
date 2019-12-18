a6c96f381516125567b3b5762dd3bbc0030a8f99
nikkesan <nikke.san@gmail.com>
2019-12-18 16:14:48 +0000

    linked cohort endpoint
    co-authored by Rosalie Baxter

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,12 +1,13 @@
-const express = require("express"); 
-const path = require("path"); 
-const router = express.Router(); 
+const express = require("express");
+const path = require("path");
+const router = express.Router();
 
 const home = require("./home");
+const cohort = require("./cohort");
 
 router.get("/", home.get);
-
-// router.use(error.client); 
+router.get("/cohort", cohort.get);
+// router.use(error.client);
 // router.use(error.server);
 
-module.exports = router; 
+module.exports = router;
