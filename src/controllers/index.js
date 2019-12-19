ec85692328f8a2257a5dfd535cb82e6f074299c0
Gillian <gillian@yeomans.org.uk>
2019-12-19 12:51:11 +0000

    add form page to add user and link up

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const home = require("./home");
 const cohort = require("./cohort");
 const userProfile = require("./userProfile");
+const addYourself = require("./addYourself");
 // const error = require("./error");
 
 router.get("/", home.get);
 router.get("/cohort", cohort.get);
 router.get("/cohort/:userProfile", userProfile.get);
+router.get("/addyourself", addYourself.get);
 // router.use(error.client);
 // router.use(error.server);
 
