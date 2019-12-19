d56a5d5f34e7c8696ad942282e72c39870d52b1a
Gillian <gillian@yeomans.org.uk>
2019-12-19 14:46:40 +0000

    add error pages for 404 and 500

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,12 +6,14 @@ const home = require("./home");
 const cohort = require("./cohort");
 const userProfile = require("./userProfile");
 const addYourself = require("./addYourself");
+const submitNewProfile = require("./submitNewProfile");
 const error = require("./error");
 
 router.get("/", home.get);
 router.get("/cohort", cohort.get);
 router.get("/cohort/:userProfile", userProfile.get);
 router.get("/addyourself", addYourself.get);
+router.post("/submitnewprofile", submitNewProfile.post);
 router.use(error.client);
 router.use(error.server);
 
---
+++ src/controllers/submitNewProfile.js
@@ -0,0 +1,3 @@
+exports.post = (req, res) => {
+  ///write me
+};
