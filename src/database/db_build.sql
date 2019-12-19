ca502e0a569a0f8a3f46425f485a2bb3583473d2
Reda <redahaq@gmail.com>
2019-12-19 17:37:02 +0000

    add css variables for colours, spacing, add reenie-beanie font, add background image

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -30,7 +30,7 @@ CREATE TABLE users (
 );
 
 
-INSERT INTO users(user_name, cohort_id, image_prof, image_party, quote, emoji, tea_coffee, hogwarts, cuisine, fruit, snack, show, js, pokemon, emojigram) VALUES 
+INSERT INTO users (user_name, cohort_id, image_prof, image_party, quote, emoji, tea_coffee, hogwarts, cuisine, fruit, snack, show, js, pokemon, emojigram) VALUES 
 ('Ayub',1,'URLHERE', 'URLHERE', '"Alright, boss!"', '👀', 'No caffeine', 'Slytherin', 'Rice and chicken', 'Pineapple', 'Fizzy laces', 'Vikings', 'Reduce', 'Charizard', '👀👀👀👀👀'),
 ('Gillian',1,'URLHERE', 'URLHERE', '""', '', 'Tea', 'Ravenclaw', 'Pub Food', 'Satsuma', 'Pickled Onion Monster Munch', 'Peep Show', 'setTimeout', 'Snorlax', '👊🐼'),
 ('Nikke',1,'URLHERE', 'URLHERE', '"Unlimited Graph Paper!"', '🤓', 'Tea', 'Ravenclaw', 'Duck and rice', 'Mangosteen', 'Chocolate Raisins', 'Fleabag', '"Why is everything an object?!"', 'Pikachu', ''),
