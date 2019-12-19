7256e0bdf457f437144fd3a28cd179eec821050e
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-12-19 18:11:41 +0000

    added userprofile page functionality

--- src/controllers/userProfile.js
+++ src/controllers/userProfile.js
@@ -1,9 +1,42 @@
-const cohort = require("./../model/index");
+const getData = require("./../queries/getData");
 
-exports.get = (req, res, next) => {
-  const { userProfile } = req.params;
-  if (cohort.includes(userProfile)) {
-    return res.render("userProfile", { userProfile });
-  }
-  next();
+exports.get = (request, response) => {
+  const { userProfile } = request.params;
+  console.log({ userProfile });
+  getData.getSpecificUser(userProfile, (err, res) => {
+    if (err) {
+      throw error;
+    } else {
+      if (!res.length) {
+        response.status(404).render("error", {
+          layout: "error",
+          statusCode: 404,
+          errorMessage: "Page not found - where the FAC is it?!",
+          err: null,
+        });
+      } else {
+        response.render("userProfile", res[0]);
+      }
+    }
+  });
 };
+
+// const getSpecificUser = (name, cb) => {
+//   dbConnection.query(`SELECT * FROM users WHERE user_name='${name}'`, (err, res) => {
+//       if (err) {
+//           cb(err);
+//       } else {
+//           cb(null, res.rows);
+//       }
+//   });
+// };
+
+// const cohort = require("./../model/index");
+
+// exports.get = (req, res, next) => {
+//   const { userProfile } = req.params;
+//   if (cohort.includes(userProfile)) {
+//     return res.render("userProfile", { userProfile });
+//   }
+//   next();
+// };
