42cfcfb2611b05a0687a745ffdfb95555edb9fb5
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-12-19 11:45:14 +0000

    merge conflict

---
+++ src/database/db_connection.js
@@ -0,0 +1,31 @@
+const { Pool } = require("pg");
+const url = require("url");
+require("dotenv").config();
+
+let CURRENT_DB = process.env.DATABASE_URL;
+console.log(CURRENT_DB);
+
+if (process.env.NODE_ENV === "test") {
+  CURRENT_DB = process.env.TEST_DATABASE_URL;
+}
+
+if (!CURRENT_DB)
+  throw new Error("Environment variable DATABASE_URL must be set");
+
+const params = url.parse(CURRENT_DB);
+
+const [username, password] = params.auth.split(":");
+
+const options = {
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+  max: process.env.DB_MAX_CONNECTIONS || 2,
+  user: username,
+  password,
+  ssl: params.hostname !== "localhost",
+};
+
+const dbConnection = new Pool(options);
+
+module.exports = dbConnection;
